0e47ad048c3b4831874e9b4f6dab842b8d89aa89
Jiwoo Lee <lee1043@llnl.gov>
2023-06-26 13:53:36 -0700

    to avoid github action error for docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,10 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import sphinx_autosummary_accessors
+
+# import pcmdi_utils
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
