88c8006a4dc30e3fb13eb13d08b3c60a2170c9ae
Jiwoo Lee <lee1043@llnl.gov>
2023-06-26 14:47:08 -0700

    test different theme

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_book_theme"
+# html_theme = "sphinx_book_theme"
+html_theme = "piccolo_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -70,6 +71,7 @@ html_theme = "sphinx_book_theme"
 # https://sphinx-book-theme.readthedocs.io/en/latest/configure.html
 html_logo = "_static/PCMDILogo_200x65px_72dpi.png"
 html_title = "PCMDI UTILS Documentation"
+html_short_title = "PCMDI UTILS"
 html_theme_options = {
     "repository_url": "https://github.com/PCMDI/pcmdi_utils",
     "repository_branch": "main",
