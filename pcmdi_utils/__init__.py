474b93154162f965d25264d672aeb05bdc7a5f10
Jiwoo Lee <lee1043@llnl.gov>
2023-06-28 11:49:25 -0700

    make internal function callable

--- pcmdi_utils/__init__.py
+++ pcmdi_utils/__init__.py
@@ -7,4 +7,8 @@ __version__ = "0.1.0"
 
 """Top-level package for pcmdi_util."""
 
-from pcmdi_utils.land_sea_mask import generate_land_sea_mask  # noqa: F401
+from pcmdi_utils.land_sea_mask import (  # noqa: F401
+    generate_land_sea_mask,
+    generate_land_sea_mask__global_land_mask,
+    generate_land_sea_mask__pcmdi,
+)
